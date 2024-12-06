2f83752da93a29f77665a00bbb5136f340a9868a
Jon Alaniz <jonalaniz@gmail.com>
2024-12-05 18:26:24 -0600

    implemented ignored folders

--- Scouter/ConfigurationViewController.swift
+++ Scouter/ConfigurationViewController.swift
@@ -14,25 +14,43 @@ class ConfigurationViewController: NSViewController {
     @IBOutlet var mailboxesPopUpButton: NSPopUpButton!
     @IBOutlet var fetchIntervalPopupButton: NSPopUpButton!
     @IBOutlet weak var versionLabel: NSTextField!
+    @IBOutlet weak var folderTableView: NSTableView!
 
     let apiService = FreeScoutService.shared
     let configurator = Configurator.shared
     var url: URL?
     var apiKey: String?
-    var ignoredFolders: Set<String>?
+    var ignoredFolders: Set<String> = []
+    var folders = [Folder]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        versionLabel.stringValue = appVersion()
-
-        guard configurator.getConfiguration() != nil else {
-            configureFetchIntervalButton()
-            return
-        }
-        
+        setupUI()
         showConfiguration()
     }
-    
+
+    private func setupUI() {
+        versionLabel.stringValue = appVersion()
+        folderTableView.delegate = self
+        folderTableView.dataSource = self
+        folderTableView.usesAlternatingRowBackgroundColors = true
+    }
+
+    private func showConfiguration() {
+        guard let config = configurator.getConfiguration() else { return }
+        configureFetchIntervalButton()
+
+        urlField.stringValue = config.secret.url.absoluteString
+        apiKeyField.stringValue = (config.secret.key)
+
+        ignoredFolders = config.ignoredFolders
+
+        getMailboxes(url: config.secret.url, apiKey: config.secret.key)
+
+        configureFetchIntervalButton()
+        fetchIntervalPopupButton.selectItem(withTag: Int(config.fetchInterval.rawValue))
+    }
+
     @IBAction func getMailboxesPressed(_ sender: Any) {
         guard let url = URL(string: urlField.stringValue) else {
             errorLabel.isHidden = false
@@ -48,34 +66,31 @@ class ConfigurationViewController: NSViewController {
         
         errorLabel.isHidden = true
         getMailboxes(url: url, apiKey: apiKeyField.stringValue)
+        ignoredFolders.removeAll()
     }
 
     @IBAction func selectionMade(_ sender: NSPopUpButton) {
+        if sender == mailboxesPopUpButton {
+            ignoredFolders.removeAll()
+            reloadTable()
+        }
+
         save()
     }
     
     func save() {
         guard let fetchInterval = FetchInterval(rawValue: TimeInterval(fetchIntervalPopupButton.selectedTag())) else { return }
         guard let id = mailboxesPopUpButton.selectedItem?.tag else { return }
-        
-        let configuration = Configuration(secret: Secret(url: url!, key: apiKey!),
-                                          fetchInterval: fetchInterval,
-                                          mailboxID: id,
-                                          ignoredFolders: ignoredFolders)
+        guard let url = url, let apiKey = apiKey
+        else { return }
 
-        configurator.saveConfiguration(configuration)
-    }
-    
-    private func showConfiguration() {
-        guard let config = configurator.getConfiguration() else { return }
-        
-        urlField.stringValue = config.secret.url.absoluteString
-        apiKeyField.stringValue = (config.secret.key)
-        
-        getMailboxes(url: config.secret.url, apiKey: config.secret.key)
-        
-        configureFetchIntervalButton()
-        fetchIntervalPopupButton.selectItem(withTag: Int(config.fetchInterval.rawValue))
+        configurator.updateConriguration(
+            url: url,
+            key: apiKey,
+            fetchInterval: fetchInterval,
+            id: id,
+            ignoredFolders: ignoredFolders
+        )
     }
     
     private func configureFetchIntervalButton() {
@@ -118,9 +133,23 @@ class ConfigurationViewController: NSViewController {
         mailboxesPopUpButton.selectItem(withTag: selected)
         mailboxesPopUpButton.isEnabled = true
 
+        configureFolderTableView(with: selected)
+
         save()
     }
-    
+
+    private func configureFolderTableView(with mailbox: Int) {
+        Task {
+            do {
+                let object = try await apiService.fetchFolders()
+                folders = object.container.folders.filter { $0.userId == nil }
+                reloadTable()
+            } catch {
+                errorLabel.stringValue = error.localizedDescription
+            }
+        }
+    }
+
     private func handle(error: APIManagerError) {
         // TODO: Handle these errors
         switch error {
@@ -131,9 +160,41 @@ class ConfigurationViewController: NSViewController {
         }
     }
 
+    // MARK: Helper Functions
     private func appVersion() -> String {
         guard let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
         else { return "Scouter v.Unknown" }
         return "Scouter v.\(version)"
+    }
+
+    @objc func boxChecked(_ sender: NSButton) {
+        switch sender.state {
+        case .on: ignoredFolders.remove(sender.title)
+        case .off: ignoredFolders.insert(sender.title)
+        default: break
+        }
+        save()
+    }
+
+    @MainActor
+    private func reloadTable() {
+        folderTableView.reloadData()
+    }
+}
+
+extension ConfigurationViewController: NSTableViewDelegate, NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return folders.count
+    }
+
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = NSButton(
+            checkboxWithTitle: folders[row].name,
+            target: self,
+            action: #selector(boxChecked)
+        )
+        cell.state = ignoredFolders.contains(folders[row].name) ? .off : .on
+
+        return cell
     }
 }
